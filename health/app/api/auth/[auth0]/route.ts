c52e21fe0b2c4f4adcb50922e1d4530cb379465a
Reagandennis <dennisowiti215@gmail.com>
2025-04-19 18:17:32 +0300

    main

--- health/app/api/auth/[auth0]/route.ts
+++ health/app/api/auth/[auth0]/route.ts
@@ -1,10 +1,11 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin, handleCallback } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
   login: handleLogin({
     returnTo: '/',
     authorizationParams: {
-      screen_hint: 'signup',  // 👈 tells Auth0 to open the Sign Up tab
+      screen_hint: 'signup',
     },
   }),
+  callback: handleCallback,  // Use default callback handling
 });
--- health/next.config.ts
+++ health/next.config.ts
@@ -7,7 +7,8 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // Or restrict to specific origins
+          { key: "Access-Control-Allow-Origin", value: "https://echopsychology.com" },
+          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000/" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type" },
         ],
